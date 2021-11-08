b3d71dc9ea63dba1f92b803ad4e790ed34567a68
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2021-11-09 04:26:20 +0530

    fixed port bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path")
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 const logger = require("./logger");
 const testRouter = require("./routes/test");

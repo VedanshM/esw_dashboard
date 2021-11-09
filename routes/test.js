12f74388452e22ffa2543d6192ec548e3fad672d
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2021-11-09 13:29:53 +0530

    om2m request sample

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+
+
+const req_options = {
+    method: 'GET',
+    url: process.env.OM2M_BASE + '/queue_data',
+    qs: { rcn: '4' },
+    headers: {
+        'accept': 'application/json',
+        'x-m2m-origin': process.env.OM2M_SECRET
+    }
+};
 
 router.get("/", (req, res) => {
-    res.send("data from backend")
+    request(req_options, (error, response, body) => {
+        if (error) {
+            console.log(error)
+            throw new Error(error);
+        }
+        res.json(body);
+    });
+
 });
 
 module.exports = router;
